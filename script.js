feabb503b61c18765a9baa6451b72bb9e57cab28
Darshan Pandya <80563657+Darshan-Pandya10@users.noreply.github.com>
2022-09-21 11:44:39 +0530

    [ADD] clean JS code and add some styling.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const board = document.querySelector('.board');
 const cells = Array.from(document.querySelectorAll('.cell'));
+let currentClass 
 const x_class = 'x';
 const circle_class = 'circle';
 const messageBox = document.querySelector('.messagebox');
 const gameMessage = document.querySelector('.gamemessage');
+const showTern = document.querySelector('.showtern');
 const restart = document.querySelector('.restart');
 const winningCombinationArray = [
     [0,1,2],
@@ -13,52 +15,63 @@ const winningCombinationArray = [
     [1,4,7],
     [2,5,8],
     [0,4,8],
-    [2,4,6]
+    [2,4,6],
 ];
-let currentClass
-
 
 restart.addEventListener('click',versusPlayer)
 
 
 function versusPlayer(){
 
+   restart.addEventListener('click' , () => {
+    restart.classList.remove('showRestartButton')
+    cells.forEach(cell => {
+        cell.classList.remove(x_class);
+        cell.classList.remove(circle_class);
+        showTern.innerText = '';
+        gameMessage.innerText = '';
+    })
+   }) ;
+
+
     function startGame(){
         cells.forEach(cell => {
             cell.addEventListener('click', clickEvent,{once:true});
-            cell.classList.remove(x_class);
-            cell.classList.remove(circle_class);
+            restart.classList.add('showRestartButton');
         });
-        gameMessage.innerText = '';
         }
         startGame()
 
-function clickEvent(e){
+    function clickEvent(e){
     const cell = e.target;
     let whosTurn = currentClass ? x_class : circle_class;
     cell.classList.add(whosTurn);
+    showTern.innerText = `${whosTurn} terns completed`;
     swapterns();
 
-    function gameEnd(draw){
-
-        if(draw){
-            gameMessage.innerText = 'Game is Draw.';
-    
-        }
-        else {
-            messageBox.classList.add('showResult');
-            gameMessage.innerText = `${whosTurn} is a Winner!`;
-        }
-        messageBox.classList.add('showResult');
-    }
     if(winnerCheck(whosTurn)){
-       gameEnd(false)
+        cells.forEach(cell => {
+            cell.removeEventListener('click', clickEvent,{once:true});
+            })
+    messageBox.classList.add('showResult');
+    gameMessage.innerText = `${whosTurn} is a Winner!`;
     }
     else if(isDraw()){
-        gameEnd(true)
+        cells.forEach(cell => {
+            cell.removeEventListener('click', clickEvent,{once:true});
+            })
+        gameMessage.innerText = 'Game is Draw.';
     }
 
+    messageBox.classList.add('showResult');
+    restart.classList.add('showRestartButton');
+
+
 }}
+
+function swapterns(){
+    currentClass = !currentClass;
+    }
 
 
 // common code
@@ -69,26 +82,14 @@ function isDraw(){
 })
 }
 
-
-function swapterns(){
-    currentClass = !currentClass;
-    }
-
-
 // function to check the winner
 function winnerCheck(whosTurn){
-
     return winningCombinationArray.some(combination => {
         return combination.every(index => {
            return cells[index].classList.contains(whosTurn);
         })
     })}
 
-function forDraw(){
-    return cells.every(cell => {
-    return cell.classList.contains(x_class) || cell.classList.contains(circle_class)
-   })
-}
 
 
 versusPlayer()
